24ee8946fc1f7ddda52b30765f3a587f560bbf14
lyaronskaya <luba.yaronskaya@gmail.com>
2014-11-23 01:01:45 +0400

    add some exceptions

--- src/ru/fizteh/fivt/students/YaronskayaLiubov/StructuredDataTables/MultiFileMapRunTimeException.java
+++ src/ru/fizteh/fivt/students/YaronskayaLiubov/StructuredDataTables/MultiFileMapRunTimeException.java
@@ -3,5 +3,8 @@ package ru.fizteh.fivt.students.YaronskayaLiubov.StructuredDataTables;
 /**
  * Created by luba_yaronskaya on 22.11.14.
  */
-public class MultiFileMapRunTimeException {
+public class MultiFileMapRunTimeException extends Exception {
+    public MultiFileMapRunTimeException(String message) {
+        super(message);
+    }
 }
