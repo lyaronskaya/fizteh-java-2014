fb0356e8459bf81c986b42adca98d00edbefd48e
lyaronskaya <luba.yaronskaya@gmail.com>
2014-12-02 01:57:38 +0400

    Add implementation of LoggingFactory and InvocationHandler

--- src/ru/fizteh/fivt/students/YaronskayaLiubov/proxy/RealInvocationHandler.java
+++ src/ru/fizteh/fivt/students/YaronskayaLiubov/proxy/RealInvocationHandler.java
@@ -1,7 +1,53 @@
 package ru.fizteh.fivt.students.YaronskayaLiubov.proxy;
 
+import java.io.Writer;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Created by luba_yaronskaya on 01.12.14.
  */
-public class RealInvocationHandler {
+public class RealInvocationHandler implements InvocationHandler {
+    private Writer writer;
+    private Object implementation;
+
+    public RealInvocationHandler(Writer writer, Object implementation) {
+        this.writer = writer;
+        this.implementation = implementation;
+    }
+
+    @Override
+    public synchronized Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+JSONObject log = new JSONObject();
+        log.put("timestamp", System.currentTimeMillis());
+        log.put("class", implementation.getClass().getName());
+        log.put("method", method.getName());
+        log.put("arguments", getJSONArray(args));
+        Throwable thrownException = null;
+        Object returnValue = null;
+        try {
+            returnValue = method.invoke(implementation, args);
+        } catch (InvocationTargetException e) {
+            thrownException = e.getTargetException();
+        }
+        if (thrownException != null) {
+            log.put("thrown", thrownException);
+        } else {
+            log.put("return value", returnValue);
+        }
+
+        writer.write(log.toString());
+
+        return returnValue;
+    }
+public JSONArray getJSONArray(Object[] args) {
+    JSONArray res = new JSONArray();
+    for (Object val : args) {
+        res.put(val);
+    }
+}
 }
--- src/ru/fizteh/fivt/students/YaronskayaLiubov/proxy/RealLoggingProxyFactory.java
+++ src/ru/fizteh/fivt/students/YaronskayaLiubov/proxy/RealLoggingProxyFactory.java
@@ -1,7 +1,42 @@
 package ru.fizteh.fivt.students.YaronskayaLiubov.proxy;
 
+import ru.fizteh.fivt.proxy.LoggingProxyFactory;
+
+import java.io.Writer;
+import java.lang.reflect.Proxy;
+
 /**
  * Created by luba_yaronskaya on 01.12.14.
  */
-public class RealLoggingProxyFactory {
+public class RealLoggingProxyFactory implements LoggingProxyFactory {
+    /**
+     * Создаёт класс-обёртку вокруг объекта <code>implementation</code>, которая при вызове
+     * методов интерфейса <code>interfaceClass</code> выполняет логирование аргументов и результата
+     * вызова методов.
+     * <p/>
+     * Класс-обёртка не имеет права выбрасывать свои исключения, но обязана выбрасывать те же самые
+     * исключения, что выбрасывает оригинальный класс.
+     * <p/>
+     * Класс-обёртка должен быть потокобезопасным.
+     *
+     * @param writer         Объект, в который ведётся запись лога.
+     * @param implementation Объект, реализующий интерфейс <code>interfaceClass</code>.
+     * @param interfaceClass Класс интерфейса, методы которого должны выполнять запись в лог.
+     * @return Объект, реализующий интерфейс <code>interfaceClass</code>, при вызове методов которого
+     * выполняется запись в лог.
+     * @throws IllegalArgumentException Если любой из переданных аргументов null или имеет некорректное значение.
+     */
+    @Override
+    public Object wrap(
+            Writer writer,
+            Object implementation,
+            Class<?> interfaceClass
+    ) {
+        if (writer == null || implementation == null || interfaceClass == null) {
+            throw new IllegalArgumentException("arguments for wrap must be not null");
+        }
+
+        return Proxy.newProxyInstance(implementation.getClass().getClassLoader(),
+                new Class[]{interfaceClass}, new RealInvocationHandler(writer, implementation));
+    }
 }
