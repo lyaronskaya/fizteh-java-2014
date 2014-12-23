fda0409b6f0508f3dd7e93068fe0f7005c754898
lyaronskaya <luba.yaronskaya@gmail.com>
2014-12-23 16:04:08 +0400

    Telnet ready

---
+++ src/ru/fizteh/fivt/students/YaronskayaLiubov/Telnet/StoreableDataTableProviderFactory.java
@@ -0,0 +1,55 @@
+package ru.fizteh.fivt.students.YaronskayaLiubov.Telnet;
+
+import ru.fizteh.fivt.storage.structured.RemoteTableProvider;
+import ru.fizteh.fivt.storage.structured.RemoteTableProviderFactory;
+import ru.fizteh.fivt.storage.structured.TableProvider;
+import ru.fizteh.fivt.storage.structured.TableProviderFactory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+/**
+ * Created by luba_yaronskaya on 16.11.14.
+ */
+
+public class StoreableDataTableProviderFactory implements RemoteTableProviderFactory, AutoCloseable {
+    private boolean closed = false;
+
+    public TableProvider create(String path) throws IOException {
+        if (closed) {
+            throw new IllegalStateException("create failed: TableProviderFactory have been closed");
+        }
+        if (path == null) {
+            throw new IllegalArgumentException("Directory is null");
+        }
+        if (path.isEmpty()) {
+            throw new IllegalArgumentException("Empty directory name");
+        }
+        return new StoreableDataTableProvider(path);
+    }
+
+    @Override
+    public void close() {
+        if (!closed) {
+            closed = true;
+        }
+    }
+
+    @Override
+    public RemoteTableProvider connect(String hostname, int port) throws IOException {
+        if (closed) {
+            throw new IllegalStateException("create failed: TableProviderFactory have been closed");
+        }
+       String path = System.getProperty("user.dir");
+        if (path == null) {
+            throw new IllegalArgumentException("Directory is null");
+        }
+        if (path.isEmpty()) {
+            throw new IllegalArgumentException("Empty directory name");
+        }
+        StoreableDataTableProvider provider = (StoreableDataTableProvider) create(path);
+        RemoteDataTableProvider remoteProvider = new RemoteDataTableProvider(provider);
+        remoteProvider.connect(hostname, port);
+        return remoteProvider;
+    }
+}
